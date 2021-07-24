220db728ca7afe7b5c7303288e57fc89281adcf3
Max Trash <admin@deadend.xyz>
2021-07-24 14:56:22 +0300

    Add upgrade menu

--- turrets/Iron_turret.gd
+++ turrets/Iron_turret.gd
@@ -7,10 +7,12 @@ enum states {
 }
 
 var current_state = states.IDLE
+var current_level = 0
 
 # must make setget methods!
 export var attack_force : float 
 export var reload_time : float
+export (Array, Resource) var animation_sprites
 
 var loaded = true
 onready var reload_timer = $Reloading_timer
@@ -18,10 +20,9 @@ onready var reload_timer = $Reloading_timer
 var watching_enemies = []
 
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_current_level(current_level)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -36,17 +37,22 @@ func _process(delta):
 			if watching_enemies.size() > 0:
 				$Turret_gun.look_at(watching_enemies[0].get_global_position())
 				if loaded:
-					shoot(watching_enemies[0])
+					$Turret_gun/Animation.play("shoot")
+					if $Turret_gun/Animation.get_frame() == 2:
+						# !!! Need check is current target is walid
+						shoot(watching_enemies[0])
 			else:
 				current_state = states.IDLE
 
 
 func shoot(attack_obj):
-	$Turret_gun/Animation.play("shoot")
-#	if $Turret_gun/Animation.get_frame() == 2:
 	attack_obj.take_damage(attack_force)
 	loaded = false
 	reload_timer.start(reload_time)
+
+
+func set_current_level(c_l):
+	$Turret_gun/Animation.set_sprite_frames(animation_sprites[c_l])
 
 
 func _on_Watch_radius_area_entered(area):
@@ -65,3 +71,22 @@ func _on_Reloading_timer_timeout():
 func _on_Animation_animation_finished():
 	$Turret_gun/Animation.stop()
 	$Turret_gun/Animation.set_frame(0)
+
+
+# Upgrade menu section
+func _on_Sell_pressed():
+	queue_free()
+
+
+func _on_Cancel_pressed():
+	pass # Replace with function body.
+
+
+func _on_Upgrade_pressed():
+	if current_level < 3:
+		current_level += 1
+		set_current_level(current_level)
+
+	if current_level == 3:
+#		$Upgrade_menu/HBox/Upgrade.set_visible(false)
+		$Upgrade_menu/HBox/Upgrade.set_disabled(true)
